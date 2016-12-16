4c6371ee06df5d9b9cd7ebf493c6d16da77e03b2
LAPTOP-0NJ0AUU6\Александра <triton1984@yandex.ru>
2016-12-16 22:48:49 +0300

    no message

--- cart.js
+++ cart.js
@@ -1,9 +1,23 @@
 // JavaScript source code
-$("body").on("click", ".btn-1", function(){
-    $(".form-cart").animate({right:"0px"}, 1000)
+$(document).ready(function () {
+
+    $("body").on("click", ".btn-1", function () {
+        $(".form-cart").animate({ right: "0px" }, 1000)
+    })
+
+    $("body").on("click", ".btn-2", function () {
+        $(".form-cart").animate({ right: "-300px" }, 1000)
+    })
+
 })
 
 
+function cart() {
+    
+    
+}
 
-})
+
+
+
 
